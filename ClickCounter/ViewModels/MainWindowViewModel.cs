7dc3efbbbbfeb57ec336a1eb5961660afd59a40c
system125 <hundredtwentyfive25@gmail.com>
2024-01-18 11:18:59 +0545

    Added Constructor

--- ClickCounter/ViewModels/MainWindowViewModel.cs
+++ ClickCounter/ViewModels/MainWindowViewModel.cs
@@ -5,5 +5,11 @@
 #pragma warning disable CA1822 // Mark members as static
         public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+        public MainWindowViewModel()
+        {
+
+        }
     }
+
+    
 }
